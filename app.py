93fb1f05aaf843f84f3e41711a93db3abc0278cd
journeykvs-commits <journeykvs@gmail.com>
2026-04-16 15:27:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "45670c82-7693-46cc-8be4-97e03cd29c83",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
